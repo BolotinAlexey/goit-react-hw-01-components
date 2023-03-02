a87aaa362fae4363fc787aa6f7d5688247920434
Oleksii Bolotin <samkiral73@gmail.com>
2023-03-02 17:07:07 +0100

    create component Profile without css & propTypes

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+export default function Profile({ username, tag, location, avatar, stats }) {
+  const { folowers, views, likes } = stats;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{folowers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
