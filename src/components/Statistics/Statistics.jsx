dda3ddf1b6cb8c72437048657c7c654c65466bd7
Oleksii Bolotin <samkiral73@gmail.com>
2023-03-03 13:03:06 +0100

    made destructuring of Staistics map

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,9 +8,9 @@ export default function Statistics({ stats }) {
       {/* <h2 className={css.title}>{title}</h2> */}
 
       <ul className={css.statList}>
-        {stats.map(el => (
-          <li key={el.id} className={css.item}>
-            <StatisticElement label={el.label} percentage={el.percentage} />
+        {stats.map(({ id, label, percentage }) => (
+          <li key={id} className={css.item}>
+            <StatisticElement label={label} percentage={percentage} />
           </li>
         ))}
       </ul>
